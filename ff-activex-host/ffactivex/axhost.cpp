11601c84f07f0c96812b6756bd521d6736d8e371
Leeor <Leeor@LEEOR-LAP.itstoffice.com>
2009-12-07 19:03:28 +0200

    fixed some changes I used for debugging

--- ff-activex-host/ffactivex/axhost.cpp
+++ ff-activex-host/ffactivex/axhost.cpp
@@ -390,7 +390,7 @@ CAxHost::CreateControl(bool subscribeToEvents)
 	}
 	else {
 
-		Site->m_bSafeForScriptingObjectsOnly = false;
+		Site->m_bSafeForScriptingObjectsOnly = true;
 	}
 
 	Site->AddRef();
